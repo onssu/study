1f389109f9b46352f9d9a4f2a90b2a971e6ac7ca
유리 <onssu1@gmail.com>
2020-05-16 13:52:30 +0900

    5/16

---
+++ HTML & JavaScript/JS/modal.js
@@ -0,0 +1,15 @@
+//필요한 엘리먼트들을 선택한다.
+const openButton = document.getElementById("open");
+const modal = document.querySelector(".modal");
+const overlay = modal.querySelector(".md_overlay");
+const closeButton = modal.querySelector("button");
+//동작함수
+const openModal = () => {
+    modal.classList.remove("hidden");
+}
+const closeModal = () => {
+    modal.classList.add("hidden");
+}
+//클릭 이벤트
+openButton.addEventListener("click", openModal);
+closeButton.addEventListener("click", closeModal);
