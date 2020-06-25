a477fd39347714ca952ca1eca21a8f25673b828f
유리 <onssu1@gmail.com>
2020-06-25 16:19:41 +0900

    6/25 react start

---
+++ react/code/movie_app/README.md
@@ -0,0 +1,1 @@
+Nomad Coder React.js
---
+++ react/code/movie_app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Potato from "./Potato";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+      <Potato />
+    </div>
+  );
+}
+
+export default App;
