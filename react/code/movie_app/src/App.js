92619f63514619f512216e3fb6a37ddd148ebeb8
유리 <onssu1@gmail.com>
2020-06-29 16:35:53 +0900

    6/29 modified

--- react/code/movie_app/src/App.js
+++ react/code/movie_app/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
-function Food(){
-  return <h1>I Like Potato</h1>;
+function Food({fav}){
+  return <h1>I Like {fav}</h1>;
 }
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <Food name="kimchi" />
+      <Food fav="kimchi" />
+      <Food fav="라면" />
+      <Food fav="햄버거" />
+      <Food fav="피자" />
+      <Food fav="치킨" />
     </div>
   );
 }
